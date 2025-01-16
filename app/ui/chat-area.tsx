6e440da4ee818eabbc0c89a90d31e0073daf4e43
seowchiayi <seowchiayi@gmail.com>
2025-01-16 13:08:04 +0800

    fix backend path

--- app/ui/chat-area.tsx
+++ app/ui/chat-area.tsx
@@ -24,7 +24,7 @@ interface ChatAreaProps {
 }
 
 const backend = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
   : "http://localhost:8000/api";
 
 export function ChatArea({ selectedConversation, selectedLLM }: ChatAreaProps) {
